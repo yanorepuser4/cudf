91fc8bab70b3dcfabb5c76fd8e73cfef83f777f7
Lawrence Mitchell <lmitchell@nvidia.com>
2024-05-22 11:54:03 +0000

    Add utility to convert from pylibcudf data type to pyarrow equivalent

--- python/cudf_polars/cudf_polars/utils/dtypes.py
+++ python/cudf_polars/cudf_polars/utils/dtypes.py
@@ -7,11 +7,14 @@ from __future__ import annotations
 
 from functools import cache
 
+import pyarrow as pa
 from typing_extensions import assert_never
 
 import polars as pl
 
 import cudf._lib.pylibcudf as plc
+
+__all__ = ["from_polars", "to_arrow"]
 
 
 @cache
@@ -84,6 +87,71 @@ def from_polars(dtype: pl.DataType) -> plc.DataType:
         # TODO: Hopefully
         return plc.DataType(plc.TypeId.EMPTY)
     elif isinstance(dtype, pl.List):
+        # TODO: This doesn't consider the value type.
         return plc.DataType(plc.TypeId.LIST)
     else:
         raise NotImplementedError(f"{dtype=} conversion not supported")
+
+
+@cache
+def to_arrow(dtype: plc.DataType) -> pa.DataType:
+    """
+    Convert a pylibcudf datatype to a pyarrow one.
+
+    Parameters
+    ----------
+    dtype
+        Pylibcudf dtype to convert
+
+    Returns
+    -------
+    Matching pyarrow type.
+
+    Raises
+    ------
+    NotImplementedError for unsupported conversions.
+    """
+    type_id = dtype.id()
+    if type_id == plc.types.TypeId.BOOL8:
+        return pa.bool_()
+    elif type_id == plc.types.TypeId.INT8:
+        return pa.int8()
+    elif type_id == plc.types.TypeId.INT16:
+        return pa.int16()
+    elif type_id == plc.types.TypeId.INT32:
+        return pa.int32()
+    elif type_id == plc.types.TypeId.INT64:
+        return pa.int64()
+    elif type_id == plc.types.TypeId.UINT8:
+        return pa.uint8()
+    elif type_id == plc.types.TypeId.UINT16:
+        return pa.uint16()
+    elif type_id == plc.types.TypeId.UINT32:
+        return pa.uint32()
+    elif type_id == plc.types.TypeId.UINT64:
+        return pa.uint64()
+    elif type_id == plc.types.TypeId.FLOAT32:
+        return pa.float32()
+    elif type_id == plc.types.TypeId.FLOAT64:
+        return pa.float64()
+    elif type_id == plc.types.TypeId.TIMESTAMP_DAYS:
+        return pa.date32()
+    elif type_id == plc.types.TypeId.TIMESTAMP_MILLISECONDS:
+        return pa.timestamp("ms")
+    elif type_id == plc.types.TypeId.TIMESTAMP_MICROSECONDS:
+        return pa.timestamp("us")
+    elif type_id == plc.types.TypeId.TIMESTAMP_NANOSECONDS:
+        return pa.timestamp("ns")
+    elif type_id == plc.types.TypeId.DURATION_MILLISECONDS:
+        return pa.duration("ms")
+    elif type_id == plc.types.TypeId.DURATION_MICROSECONDS:
+        return pa.duration("us")
+    elif type_id == plc.types.TypeId.DURATION_NANOSECONDS:
+        return pa.duration("ns")
+    elif type_id == plc.types.TypeId.STRING:
+        return pa.string()
+    elif type_id == plc.types.TypeId.EMPTY:
+        return pa.null()
+    else:
+        # TODO: LIST needs to provide value_type inside.
+        raise NotImplementedError(f"{type_id=} conversion not supported")
